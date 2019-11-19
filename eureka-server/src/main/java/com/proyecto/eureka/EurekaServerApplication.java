956b471874c9dc61c7aa36de8b8adaaf8dee64c8
749706 <749706@45HW010462.SOAM.TCS.com>
2019-11-18 22:32:32 -0500

    Two Commit

--- eureka-server/src/main/java/com/proyecto/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/proyecto/eureka/EurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.proyecto.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
